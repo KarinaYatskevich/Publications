4c0946773277659ffc0c9e1e2585857e6511e6f9
karina <karinav19817@gmail.com>
2022-09-20 19:26:32 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,74 @@
-const modalWindowElement = document.getElementById("modal-window");
-const btnElements = document.querySelectorAll(".button");
-const closeElement = document.getElementsByClassName("close")[0];
+// UTILS
 
-btnElements.forEach(function(btn) {
-    btn.onclick = function() {
-        modalWindowElement.style.display = "block";
+class Observer {
+    #callbacks = []
+
+    subscribe(callback) {
+        this.#callbacks.push(callback)
     }
-});
-closeElement.onclick = function() {
-    modalWindowElement.style.display = "none";
-}
 
-window.onclick = function(event) {
-    if (event.target == modalWindowElement) {
-        modalWindowElement.style.display = "none";
+    broadcast() {
+        this.#callbacks.forEach(callback => {
+            callback()
+        })
     }
 }
+
+class Store extends Observer {
+    value;
+
+    constructor(initialValue) {
+        super()
+        this.value = initialValue;
+    }
+
+    update(nextValue) {
+        this.value = nextValue;
+        this.broadcast();
+    }
+}
+
+// STATE
+
+const app = {
+    hasModalWindow: false,
+    publications: []
+}
+
+const appStore = new Store(app);
+
+function getPublicationsCount() {
+    return app.publications.length
+}
+
+function isHasPublications() {
+    return getPublicationsCount() > 0
+}
+
+// EVENTS
+document.getElementById('add-button').addEventListener('click', () => {
+    app.hasModalWindow = true;
+    appStore.broadcast()
+})
+
+document.getElementById('create-button').addEventListener('click', () => {
+    app.hasModalWindow = true;
+    appStore.broadcast()
+})
+
+document.getElementById('close').addEventListener('click', () => {
+    app.hasModalWindow = false;
+    appStore.broadcast()
+})
+
+appStore.subscribe(() => {
+    if (app.hasModalWindow) {
+        document.getElementById('modal-window').style.display = 'block';
+    } else {
+        document.getElementById('modal-window').style.display = 'none';
+    }
+})
+
+appStore.subscribe(() => {
+    document.getElementById('publications-count').innerHTML = getPublicationsCount() + " publications";
+})
